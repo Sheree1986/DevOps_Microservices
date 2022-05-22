e4e82895141e87f9bd2367fc0af9a8f08d295905
Sheree1986 <sheree.edmund@gmail.com>
2022-05-21 22:29:28 -0400

    Step 7 push to github

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mymicroservice_api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run mymicroserviceapi --image=sheree1986/mymicroservice_api --port=80
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mymicroserviceapi 8000:80 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=mymicroservice_api
 # Step 2:  
 # Authenticate & tag
+docker login --username sheree1986
+docker tag mymicroservice_api sheree1986/mymicroservice_api
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push sheree1986/mymicroservice_api:latest
